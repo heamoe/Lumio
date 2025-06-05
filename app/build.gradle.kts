ce4da1bec47346dea6af1f28687f72277eb070dd
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2025-06-05 15:19:04 -0400

    add navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity)
+    implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
